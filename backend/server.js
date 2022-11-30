d1697d23442d66b9656374b6ee12b2e090476787
Mark Jerly Bundalian <mjbundalian61@gmail.com>
2022-11-30 23:29:02 +0800

    Add connection to Mongo DB in Cloud

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,15 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 const workoutRoutes = require('./routes/workouts')
 
 // config dotenv
 dotenv.config()
+
+// Connect to Mongo DB
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => console.log("Connected to db"))
+    .catch((err) => console.log(err))
 
 // Express app
 const app = express()
