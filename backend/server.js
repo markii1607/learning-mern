316a0f3401b70e94ce8d5b5c66637b941d79c7d6
Mark Jerly Bundalian <mjbundalian61@gmail.com>
2022-11-30 23:14:18 +0800

    Add routes for workouts

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const dotenv = require('dotenv')
+const workoutRoutes = require('./routes/workouts')
 
 // config dotenv
 dotenv.config()
@@ -7,16 +8,16 @@ dotenv.config()
 // Express app
 const app = express()
 
-// Middleware to log all requests
+// Middlewares
+app.use(express.json())
+
 app.use((req, res, next) => {
     console.log(req.path, req.method)
     next()
 })
 
-// Routes
-app.get('/', (req, res) => {
-    res.json({message: "Welcome to the app"})
-})
+// Routes for APIs
+app.use('/api/workouts', workoutRoutes)
 
 // Listener
 app.listen(process.env.PORT, () => console.log("Listening to port 3333"))
