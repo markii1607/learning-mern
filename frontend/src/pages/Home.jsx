eba34de1ea9e420d0c06c7a02137eb90e8597164
Mark Jerly Bundalian <mjbundalian61@gmail.com>
2022-12-07 22:50:08 +0800

    Create a proxy for CORS fetch on backend

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,13 +1,26 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 
 const Home = () => {
-    useEffect(() => {
+    const [workouts, setWorkouts] = useState(null)
 
+    useEffect(() => {
+        const fetchWorkouts = async () => {
+            const response = await fetch('/api/workouts')
+            const json = await response.json()
+
+            if (response.ok) {
+                setWorkouts(json)
+            }
+        }
+
+        fetchWorkouts()
     }, [])
 
     return (
         <div className="home">
-            <h2>Home</h2>
+            {workouts && workouts.map(workout => (
+                <p key={workout._id}>{workout.title}</p>
+            ))}
         </div>
     )
 }
